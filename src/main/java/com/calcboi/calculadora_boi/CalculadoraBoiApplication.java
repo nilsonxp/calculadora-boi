ce581a02bf3b458ddf8c0259ceb51da22fa57e3b
nilsonxp <nilsonxp@gmail.com>
2025-03-01 20:48:17 -0400

    first commit

---
+++ src/main/java/com/calcboi/calculadora_boi/CalculadoraBoiApplication.java
@@ -0,0 +1,13 @@
+package com.calcboi.calculadora_boi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraBoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraBoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calcboi/calculadora_boi/CalculadoraBoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcboi.calculadora_boi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraBoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
