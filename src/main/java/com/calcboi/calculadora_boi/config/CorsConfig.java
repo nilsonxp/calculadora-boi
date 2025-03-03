109e68449ca93ede73a2ef1e172cdb0d35fa4313
nilsonxp <nilsonxp@gmail.com>
2025-03-03 17:52:47 -0400

    fix Cors para aceitar o front com a permissão de origem corrigida

--- src/main/java/com/calcboi/calculadora_boi/config/CorsConfig.java
+++ src/main/java/com/calcboi/calculadora_boi/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://seu-frontend.netlify.app") // Permita apenas o frontend correto
+                .allowedOrigins("https://calculadora-boi-production.up.railway.app") // Permita apenas o frontend correto
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
